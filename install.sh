9899c0192c0127f13744f37f4bd45f152c3299cd
Minkyu Cho <mrnoname@naver.com>
2016-03-30 11:31:51 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # find path for IntelliJ preferences directory (can be multiple)
-I=$(find ~/Library/Preferences -type d -name IntelliJIdea* -o -type d -name IdeaIC*)
-
+IFS=$'\n' read -rd '' -a I <<< $(find ~/Library/Preferences -type d -name IntelliJIdea* -o -type d -name IdeaIC*)
 # execute `curl` to save keymap
 for temp in $I; do
-	X="$I/keymaps/Keymap_Xcode.xml"
-	echo Installing as $X
-	curl "https://raw.githubusercontent.com/Pitzcarraldo/IntelliJ-IDEA-Xcode-Keymap/master/Keymap_Xcode.xml" > "$X"
+        X="$temp/keymaps/Keymap_Xcode.xml"
+        echo Installing as $X
+        cp ./Keymap_Xcode.xml "$X"
 done
